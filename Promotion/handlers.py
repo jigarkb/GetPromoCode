00e2f9242c253b7975de85a6ddc5724d80e22a7a
Jigar Bhatt <jigarbhatt93@gmail.com>
2018-01-16 18:48:22 -0800

    incorporated memcache for faster results

--- Promotion/handlers.py
+++ Promotion/handlers.py
@@ -9,24 +9,38 @@ from google.appengine.ext.webapp import template
 from .models import Promotion
 import utils
 
+from memcacheWrapper import memcachePlus
+
 
 class PromotionHandler(webapp2.RequestHandler):
     def get(self, company):
-        company_promo = Promotion.get(company_keyname=company)
-        if not company_promo:
-            self.error(404)
-            return
+        cache_key = "PromotionHandler.get.{}".format(company)
+        cached_results = memcachePlus.get(cache_key)
+        if cached_results:
+            template_values = json.loads(cached_results)
+        else:
+            company_promo = Promotion.get(company_keyname=company)
+            if not company_promo:
+                self.error(404)
+                return
 
-        template_values = Promotion.get_json_object(company_promo[0])
+            template_values = Promotion.get_json_object(company_promo[0])
+            memcachePlus.set(cache_key, json.dumps(template_values))
 
         page = utils.template("promo.html", "Promotion/html")
         self.response.out.write(template.render(page, template_values))
 
     def fetch_all(self):
-        all_promo = Promotion.get()
-        result = []
-        for company_promo in all_promo:
-            result.append(Promotion.get_json_object(company_promo))
+        cache_key = "PromotionHandler.fetch_all"
+        cached_results = memcachePlus.get_multipart(cache_key)
+        if cached_results:
+            result = json.loads(cached_results)
+        else:
+            all_promo = Promotion.get()
+            result = []
+            for company_promo in all_promo:
+                result.append(Promotion.get_json_object(company_promo))
+            memcachePlus.set_multipart(cache_key, json.dumps(result))
 
         template_values = {"all_promo": result}
 
@@ -42,6 +56,7 @@ class PromotionHandler(webapp2.RequestHandler):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
 
         try:
+            cache_key = "PromotionHandler.fetch_all"
             promotion = Promotion()
             response = promotion.add(
                 company_keyname=self.request.get("company_keyname", None),
@@ -53,6 +68,7 @@ class PromotionHandler(webapp2.RequestHandler):
                 promo_type=self.request.get("promo_type", None),
                 promo_note=self.request.get("promo_note", None),
             )
+            memcachePlus.delete_multipart(cache_key)
             self.response.out.write(json.dumps({'success': True, 'error': [], 'response': response}))
         except Exception as e:
             self.response.out.write(json.dumps({'success': False, 'error': e.message, 'response': None}))
